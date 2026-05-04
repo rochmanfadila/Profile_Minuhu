cd2fc4640069ca59a40f78e6d449f3db1eef9b8b
Moch Rochman Fadila Faqih <kecapsam69@gmail.com>
2026-05-05 01:24:06 +0700

    update navbar

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -3,8 +3,9 @@ export default function Navbar({ profile }) {
     <nav className="bg-green-800 text-white p-4 sticky top-0">
       <div className="max-w-6xl mx-auto flex justify-between">
         <h1 className="font-bold">
-          {profile.nama_sekolah || "MI NURUL HUDA"}
+          {profile?.nama_sekolah || "MI NURUL HUDA"}
         </h1>
+
         <div className="space-x-6 text-sm">
           <a href="#profil">Profil</a>
           <a href="#berita">Berita</a>
